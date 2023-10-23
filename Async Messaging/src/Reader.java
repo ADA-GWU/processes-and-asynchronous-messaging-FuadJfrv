b31051a78471b7778b29daa980e7b0daf697d823
FuadJfrv <fjafarov11530@ada.edu.az>
2023-10-23 18:10:40 +0400

    Testing out JDBC

--- Async Messaging/src/Reader.java
+++ Async Messaging/src/Reader.java
@@ -1,7 +1,19 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class Reader
 {
-    public static void main(String[] args)
+    public static void main(String[] args) throws SQLException
     {
+        String sql = "select name from product where id=8";
+        String url = "jdbc:postgresql://localhost:5432/dbtest";
+        String username = "postgres";
+        String password = "1234";
 
+        Connection con = DriverManager.getConnection(url, username , password);
+        Statement st = con.createStatement();
+        st.executeQuery(sql);
     }
 }
