070b2f9948b3279ff0e8ec6bb87848916b3a369a
FuadJfrv <fjafarov11530@ada.edu.az>
2023-10-28 23:50:51 +0400

    Added threads

--- Async Messaging/src/Sender.java
+++ Async Messaging/src/Sender.java
@@ -5,35 +5,18 @@ public class Sender
 {
     public static void main(String[] args) throws SQLException
     {
-        /*
-        dbs = db_list;
-        for each (db in db_list) {
-
-         */
-
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter a message: ");
-        String message = scanner.nextLine();
-
-        String senderName = "Fuad";
-
-        Timestamp t = new Timestamp(System.currentTimeMillis());
-
-
 
         String url = "jdbc:postgresql://localhost:5432/asyncdb";
         String username = "postgres";
-        String password = "1234";
+        String password = "fufu9999";
 
-        Connection con = DriverManager.getConnection(url, username , password);
+        while (true) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Enter a message: ");
+            String message = scanner.nextLine();
 
-        String sql = "INSERT INTO as_messages(sender_name, message, sent_time) VALUES(?, ?, ?)";
-        PreparedStatement pst = con.prepareStatement(sql);
-        pst.setString(1, senderName);
-        pst.setString(2, message);
-        pst.setTimestamp(3, t);
-        pst.executeUpdate();
-
-        con.close();
+            SenderThread thread1 = new SenderThread(url, username, password, message);
+            thread1.start();
+        }
     }
 }
---
+++ Async Messaging/src/SenderThread.java
@@ -0,0 +1,42 @@
+import java.sql.*;
+
+public class SenderThread extends Thread
+{
+    String url;
+    String username;
+    String password;
+    String sql = "INSERT INTO as_messages(sender_name, message, sent_time) VALUES(?, ?, ?)";
+    String senderName = "Fuad";
+    String message;
+
+    public SenderThread(String url, String username, String password, String message) {
+        this.url = url;
+        this.username = username;
+        this.password = password;
+        this.message = message;
+    }
+
+    @Override
+    public void run() {
+        Connection con = null;
+        try
+        {
+            Timestamp t = new Timestamp(System.currentTimeMillis());
+
+            con = DriverManager.getConnection(url, username , password);
+
+            PreparedStatement pst = con.prepareStatement(sql);
+            pst.setString(1, senderName);
+            pst.setString(2, message);
+            pst.setTimestamp(3, t);
+            pst.executeUpdate();
+            //System.out.println("sent");
+            con.close();
+
+        } catch (SQLException e)
+        {
+            throw new RuntimeException(e);
+        }
+
+    }
+}
